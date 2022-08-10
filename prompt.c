7599e758e55f544f5616252aaab48ca2fa0edf6d
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-08-10 23:51:30 +0100

    fixing betty bugs

---
+++ prompt.c
@@ -0,0 +1,20 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "shell.h"
+#include <unistd.h>
+/**
+ * prompt - to get started or file indirection used
+ * Return: Nothing
+ */
+void prompt(void)
+{
+	int int_mode;
+
+	int_mode = isatty(STDIN_FILENO);
+	if (int_mode == 1)
+		write(STDOUT_FILENO, "$", 1);
+}
+
+
